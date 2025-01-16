78c68cf5dcbd50fd25681251603abdabee629f6b
durotolu <durolawk@gmail.com>
2025-01-16 13:14:13 +0100

    set up frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include your source files
+    "./public/index.html",        // If using a single HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
